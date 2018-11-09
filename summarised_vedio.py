8801c41e5315d677ed03a5ae91f0c16b6cdf5ac9
Shashank V Hiremath <shashankhire2@gmail.com>
2018-11-09 12:34:40 +0530

    Add GUI. made Object Oriented

--- summarised_vedio.py
+++ summarised_vedio.py
@@ -13,12 +13,14 @@ import numpy as np
 import imutils
 from random import randint
 
-count=0
-while count<673:  #These the number of frames that are left after summarisation of the video.Note:Change this to the summarised_count which is available after running summarisation_trail.py
-    frame1=cv2.imread("photoo{}.png".format(count))
-    cv2.imshow("summarised",frame1)
-    count=count+1
-    k=cv2.waitKey(1)
-    if k==27:
-        cv2.destroyAllWindows()
-cv2.destroyAllWindows()
+class c2:
+	def f2(self,summarised_framecount):
+		count=0
+		while count<summarised_framecount:  #These the number of frames that are left after summarisation of the video.Note:Change this to the summarised_count which is available after running summarisation_trail.py
+		    frame1=cv2.imread("photoo{}.png".format(count))
+		    cv2.imshow("summarised",frame1)
+		    count=count+1
+		    k=cv2.waitKey(1)
+		    if k==27:
+		        cv2.destroyAllWindows()
+		cv2.destroyAllWindows()
